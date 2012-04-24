98b6e122d4c16294af29544c1d224ae38e6a4c93
Andres Riofrio <riofrios@gmail.com>
2012-04-23 20:53:17 -0700

    Move code from bin to lib so that we don't need a bin/oration.rb (DRY), so that tests pass.

--- test/ts_all.rb
+++ test/ts_all.rb
@@ -1,5 +1,9 @@
 # Programmer: Chris Bunch
 
+require 'test/unit'
+
 $:.unshift File.join(File.dirname(__FILE__))
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
 require 'test_oration'
 require 'test_generator'
