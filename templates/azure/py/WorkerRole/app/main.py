f3cdae498f1ea6ecb5f636aae36575c15ec53009
Andres Riofrio <riofrios@gmail.com>
2012-05-10 17:24:35 -0700

    Extract "create connection" code into a module

--- templates/azure/py/WorkerRole/app/main.py
+++ templates/azure/py/WorkerRole/app/main.py
@@ -20,19 +20,10 @@ import base64
 import httplib
 httplib.HTTPConnection.debuglevel = 1
 
-# NOTE: We use blob storage instead of db because `winazurestorage` does not
-# support editing Table Storage, only Blob Storage.
-import winazurestorage as azure
+import azure
 import webapp2
 
 import {{ package_name }}
-
-blob = None
-if 'AZURE_STORAGE_ACCOUNT_NAME' in os.environ and 'AZURE_STORAGE_ACCESS_KEY' in os.environ \
-    and os.environ['AZURE_STORAGE_ACCOUNT_NAME'] and os.environ['AZURE_STORAGE_ACCESS_KEY']:
-  blob = azure.BlobStorage(azure.CLOUD_BLOB_HOST, os.environ['AZURE_STORAGE_ACCOUNT_NAME'], os.environ['AZURE_STORAGE_ACCESS_KEY'])
-else:
-  blob = azure.BlobStorage() # use local dev storage
 
 def get_container(namespace):
   name = "cicero-{{ app_id }}" + namespace
