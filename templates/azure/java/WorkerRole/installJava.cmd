f3c3617a4108da709718d32cbdd1a6d59f23ceaa
Andres Riofrio <riofrios@gmail.com>
2012-05-30 05:25:37 -0700

    Use 7za to decompress instead of Powershell
    For some reason, the Powershell solution doesn't do anything in Azure.
    It might have something to do with the popup...

--- templates/azure/java/WorkerRole/installJava.cmd
+++ templates/azure/java/WorkerRole/installJava.cmd
@@ -7,10 +7,9 @@ REM Download from our S3 bucket
 if not exist jdk.zip powershell -c "(new-object System.Net.WebClient).DownloadFile('https://s3-ap-southeast-1.amazonaws.com/ariofrio-singapore/jdk-6u32-windows-x64.zip', 'jdk.zip')"
 
 REM Install the JDK
-REM http://digitalsanctum.com/2008/06/13/silent-install-of-jdk-and-jre/
-REM start /w jdk.exe /s /v "/qn INSTALLDIR=%JAVA_HOME% REBOOT=Supress"
-REM http://serverfault.com/a/201604/119041
-powershell -c "$zip = (new-object -com shell.application).namespace((Get-Location).Path + '\jdk.zip'); (new-object -com shell.application).namespace($env:java_home).Copyhere($zip.items())"
+pushd %JAVA_HOME%
+%~dp0\7za x %~dp0\jdk.zip -y
+popd
 
 REM Ensure permissive ACLs so other users (like the one that's about to run Python) can use everything.
 icacls "%JAVA_HOME%" /grant everyone:f
