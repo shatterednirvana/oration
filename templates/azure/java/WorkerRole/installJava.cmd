4329ddc063aa5b5adc193faee96e76e66cb4553c
Andres Riofrio <riofrios@gmail.com>
2012-05-29 20:31:27 -0700

    Use proper parameters for JDK installation

--- templates/azure/java/WorkerRole/installJava.cmd
+++ templates/azure/java/WorkerRole/installJava.cmd
@@ -9,7 +9,8 @@ cd /d "%~dp0"
 REM Download from our S3 bucket
 powershell -c "(new-object System.Net.WebClient).DownloadFile('https://s3.amazonaws.com/procure-language-java/jdk-6u31-windows-x64.exe', 'jdk.exe')"
 
-start /w jdk.exe /qn /norestart TARGETDIR="%JAVA_HOME%"
+REM http://digitalsanctum.com/2008/06/13/silent-install-of-jdk-and-jre/
+start /w jdk.exe /s /v "/qn INSTALLDIR=%JAVA_HOME% REBOOT=Supress"
 
 REM Ensure permissive ACLs so other users (like the one that's about to run Python) can use everything.
 icacls "%JAVA_HOME%" /grant everyone:f
