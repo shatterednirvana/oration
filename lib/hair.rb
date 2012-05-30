fd7ad8a18afb6d3649ed24fde1ad788af2a9b39f
Andres Riofrio <riofrios@gmail.com>
2012-05-29 22:51:45 -0700

    Add support for binary files in templating engine

--- lib/hair.rb
+++ lib/hair.rb
@@ -1,20 +1,28 @@
 require 'mustache'
+require 'ptools'
 
 class Hair
   class << self
     # Render all the templates in the directory `source` using the given
     # context and put the results in the directory `target`.
+    #
+    # Copy binary files as-is.
     def render(source, target, ctx = {})
       target = File.expand_path(target)
 
       Dir.chdir(source) do
         Dir['**/*'].each do |name|
           next if File.directory?(name)
-          rendered = Mustache.render(File.read(name), ctx)
+          target_dir = File.join(target, File.dirname(name))
+          FileUtils.mkdir_p(target_dir)
 
-          target_file = File.join(target, name)
-          FileUtils.mkdir_p(File.dirname(target_file))
-          File.open(target_file, 'w') {|f| f.write(rendered)}
+          if File.binary? name
+            FileUtils.cp name, target_dir
+          else
+            rendered = Mustache.render(File.read(name), ctx)
+            target_file = File.join(target, name)
+            File.open(target_file, 'w') {|f| f.write(rendered)}
+          end
         end
       end
     end
