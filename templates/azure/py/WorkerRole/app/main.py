f349116dbc8c6e22f63894bf5326fa57e9dbdce8
Andres Riofrio <riofrios@gmail.com>
2012-07-26 16:39:47 -0700

    Stop using non-URL-safe output locations

--- templates/azure/py/WorkerRole/app/main.py
+++ templates/azure/py/WorkerRole/app/main.py
@@ -50,7 +50,7 @@ class TaskRoute(webapp2.RequestHandler):
     output = ''
     if self.request.get('output') == '':
       key_length = 16  # for now, randomly generates keys 16 chars long
-      json_data['output'] = base64.b64encode(os.urandom(key_length))  # TODO - does this work in app engine?
+      json_data['output'] = base64.b64encode(os.urandom(key_length), '-_')  # TODO - does this work in app engine?
     else:
       json_data['output'] = str(self.request.get('output'))
     output = str(json_data['output'])
